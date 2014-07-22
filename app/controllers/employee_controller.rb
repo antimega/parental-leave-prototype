1d26c1b2610750243267b292318bef1aa59e842a
Chris Heathcote <chris.heathcote@digital.cabinet-office.gov.uk>
2014-07-22 10:30:39 +0100

    Changes breadcrumbs, some view names

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,18 +1,48 @@
 class EmployeeController < ApplicationController
 	def index
-		@title = 'Parental pay and leave claculator'
+		@title = 'Parental pay and leave calculator'
 		@has_breadcrumbs = true
 		@has_related_links = true
 	end
 
 	def circumstances
-		@title = 'Parental pay and leave claculator'
+		@title = 'Parental pay and leave calculator'
+			@has_breadcrumbs = true
+			@has_related_links = false
+	end
+
+	def duedate
+		@title = 'Parental pay and leave calculator'
+			@has_breadcrumbs = true
+			@has_related_links = false
+	end
+
+	def employment
+		@title = 'Parental pay and leave calculator'
+			@has_breadcrumbs = true
+			@has_related_links = false
+	end
+
+	def leavedates
+		@title = 'Parental pay and leave calculator'
+			@has_breadcrumbs = true
+			@has_related_links = false
+	end
+
+	def mothersemployment
+		@title = 'Parental pay and leave calculator'
+			@has_breadcrumbs = true
+			@has_related_links = false
+	end
+
+	def partnersemployment
+		@title = 'Parental pay and leave calculator'
 			@has_breadcrumbs = true
 			@has_related_links = false
 	end
 
 	def outcome
-		@title = 'Parental pay and leave claculator'
+		@title = 'Parental pay and leave calculator'
 		@has_breadcrumbs = true
 		@has_related_links = true
 	end
